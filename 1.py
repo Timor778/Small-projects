b67936ec35f80799c904708b7c64c406a9460cbf
Timor778 <978270681@qq.com>
2022-05-28 10:12:12 +0800

    1

---
+++ 1.py
@@ -0,0 +1,10 @@
+import numpy as np
+a = np.array([[12, 3.2, 3]], dtype=np.int32)
+print('a.ndim:',a.ndim)
+print('a.shape:',a.shape)
+print('a.size:',a.size)
+print('a.dtype:',a.dtype)
+print('a.itemsize:',a.itemsize)
+b = np.arange(1,5,1)
+print(b)
+
